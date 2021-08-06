89bb9b367e9828c49178ee4287d98ddb01e07de9
eugepineiro <eugeniasolp@gmail.com>
2021-08-06 12:09:48 -0300

    added config

---
+++ src/main/java/Particle.java
@@ -0,0 +1,40 @@
+
+public class Particle {
+    private long x;
+    private long y;
+    private long radius;
+
+    public long getX() {
+        return x;
+    }
+
+    public long getY() {
+        return y;
+    }
+
+    public long getRadius() {
+        return radius;
+    }
+
+    public void setX(long x) {
+        this.x = x;
+    }
+
+    public void setY(long y) {
+        this.y = y;
+    }
+
+    public void setRadius(long radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Particle{" +
+                "x=" + x +
+                ", y=" + y +
+                ", radius=" + radius +
+                '}';
+    }
+}
+
