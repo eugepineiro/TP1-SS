18b9da855fc6c93fff7ebf2578c7db5060a514f8
eugepineiro <eugeniasolp@gmail.com>
2021-08-09 15:45:26 -0300

    added plot

--- postprocessing/main.py
+++ postprocessing/main.py
@@ -1,12 +1,47 @@
+import json, random
+
+from numpy import number
 from plotter import plot
 
-x = [2,3,4,5]
-y = [2,3,4,5]
-radius = [200,300,400,500]
-particle = [1,1,10] # x, y, rc
-interaction_radius = 150 
-neighbours_x = [2,3]
-neighbours_y = [2,3]
-neighbours_radius = [200,300]
+with open("../src/main/resources/config/config.json") as f:
+    config = json.load(f) 
 
-plot(x,y, radius, particle, interaction_radius, [neighbours_x, neighbours_y, neighbours_radius])
+with open("../src/main/resources/result.json") as f:
+    all_particles = json.load(f) 
+
+interaction_radius = config["r_interaction_radius"] 
+number_of_particles = config["n_number_of_particles"]
+idx = random.randint(0,number_of_particles-1)
+
+particle = all_particles[idx]
+print("ID: ", particle['id'])
+
+all_particles_x = []
+all_particles_y = []
+all_particles_radius = []
+neighbours_x = []
+neighbours_y = []
+neighbours_radius = []
+
+
+for i in range(number_of_particles): 
+    if(i != idx):
+        all_particles_x.append(all_particles[i]['x'])
+        all_particles_y.append(all_particles[i]['y'])
+        all_particles_radius.append(all_particles[i]['radius'])
+
+for i in range(len(all_particles[idx]['neighbours'])): 
+    neighbours_x.append(all_particles[idx]['neighbours'][i]['x'])
+    neighbours_y.append(all_particles[idx]['neighbours'][i]['y'])
+    neighbours_radius.append(all_particles[idx]['neighbours'][i]['radius'])    
+
+
+#x = [2,3,4,5]
+#y = [2,3,4,5]
+#radius = [200,300,400,500]
+#particle = [1,1,10] # x, y, rc 
+#neighbours_x = [2,3]
+#neighbours_y = [2,3]
+#neighbours_radius = [200,300]
+
+plot(all_particles_x,all_particles_y, all_particles_radius, particle, interaction_radius, [neighbours_x, neighbours_y, neighbours_radius])
--- postprocessing/plotter.py
+++ postprocessing/plotter.py
@@ -1,12 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+SCALE = 50
 
 def plot(particles_x, particles_y, particles_radius, particle, interaction_radius, neighbours ): 
    
-    plt.scatter(particle[0], particle[1], s=particle[2], color='red', alpha=0.5)                #particle
-    plt.scatter(particle[0], particle[1], s=interaction_radius,color="none", edgecolor="red")   #interaction radius 
-    plt.scatter(particles_x, particles_y, s=particles_radius, alpha=0.5)                        #other partciles
-    plt.scatter(neighbours[0], neighbours[1], s=neighbours[2],color="green")
+
+
+    plt.scatter(particle['x'], particle['y'], s=particle['radius']*SCALE, color='red', alpha=0.5)         #particle
+    plt.scatter(particle['x'], particle['y'], s=interaction_radius*SCALE,color="none", edgecolor="black")   #interaction radius 
+    plt.scatter(particles_x, particles_y, s=[e * SCALE for e in particles_radius], alpha=0.5)                            #other partciles
+    plt.scatter(neighbours[0], neighbours[1], s=[e * SCALE for e in neighbours[2]],color="green", alpha=0.5)
     plt.grid(color='#CCCCCC')
     plt.show()
